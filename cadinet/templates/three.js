b9fdd9b63dffa929c119c5a01453cadb3933c50b
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-08-11 13:17:47 +0200

    improved 3D view

--- cadinet/templates/three.js
+++ cadinet/templates/three.js
@@ -1,14 +1,10 @@
 var camera, controls, scene, renderer;
 function attach_renderer(target) {
-
 	var SCREEN_WIDTH = 750, SCREEN_HEIGHT = 400;
 	var VIEW_ANGLE = 35, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-	target.appendChild( renderer.domElement );
-
 	scene = new THREE.Scene();
+
 	camera = new THREE.PerspectiveCamera(
 		VIEW_ANGLE, // Field of view
 		ASPECT, // Aspect ratio
@@ -28,7 +24,7 @@ function attach_renderer(target) {
 	controls.dynamicDampingFactor = 0.3;
 	controls.keys = [ 65, 83, 68 ];
 
-	//vertex data
+
 	var geom = new THREE.Geometry();
 	{% for v in vertices %}
 	geom.vertices.push(new THREE.Vector3({{ v[0] }},{{ v[1] }},{{ v[2] }}));
@@ -37,18 +33,21 @@ function attach_renderer(target) {
 	geom.faces.push(new THREE.Face3({{ f[0] }},{{ f[1] }},{{ f[2] }}));
 	{%- endfor %}
 
-	geom.computeFaceNormals();
+	geom.computeFaceNormals()
 
-	var basematerial = new THREE.MeshPhongMaterial( { color: 0x888888 } );
+	var basematerial = new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0x555555, specular: 0x666666, shininess: 10 } );
+
 	var mesh = new THREE.Mesh( geom, basematerial );
 	scene.add( mesh );
 
-	var d1 = new THREE.DirectionalLight(0xffffff);
-	d1.position.set(1,1,1).normalize();
-	scene.add(d1);
-	var d2 = new THREE.DirectionalLight(0xffffff);
-	d2.position.set(-1,-1,-1).normalize();
-	scene.add(d2);
+	var d1 = new THREE.DirectionalLight(0xffffff); d1.position.set(1,1,1).normalize(); scene.add(d1);
+	var d2 = new THREE.DirectionalLight(0xffffff); d2.position.set(-1,-1,-1).normalize(); scene.add(d2);
+
+	renderer = new THREE.WebGLRenderer({ antialias : true });
+	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+	renderer.setClearColor(0xffffff, 1)
+
+	target.appendChild( renderer.domElement );
 
 	renderer.render( scene, camera );
 	animate();
